ad5cfcacd34ce2d64000ef546127196482990909
Jhonatan <jrios.orsis@gmail.com>
2025-08-16 17:44:04 -0500

    Fixing brackets in kms policy

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ data "aws_iam_policy_document" "default" {
       type        = "AWS"
       identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
     }
-    actions   = "kms:*"
+    actions   = ["kms:*"]
     resources = ["*"]
   }
 }
